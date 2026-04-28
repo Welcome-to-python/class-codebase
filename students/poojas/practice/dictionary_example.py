58e43fe1912e2f953524d12408332e0b16cb3c0b
poojas <poojas27018@gmail.com>
2026-04-28 18:07:50 +0530

    dictionary_example

---
+++ students/poojas/practice/dictionary_example.py
@@ -0,0 +1,9 @@
+student = {
+    "name": "Pooja",
+    "age": 18,
+    "course": "Engineering"
+}
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
