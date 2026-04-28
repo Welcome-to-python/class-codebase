d137d54b1caad0c53f33211125bb6d3d3a5fecdb
vastradpavan88 <vastradpavan@gmail.com>
2026-04-28 19:46:34 +0530

    added q3 to q20

---
+++ students/PavanVastrad/q13_duplicates_list.py
@@ -0,0 +1,9 @@
+lst = [1, 2, 3, 2, 4, 1, 5]
+
+duplicates = []
+
+for item in lst:
+    if lst.count(item) > 1 and item not in duplicates:
+        duplicates.append(item)
+
+print("Duplicates:", duplicates)
