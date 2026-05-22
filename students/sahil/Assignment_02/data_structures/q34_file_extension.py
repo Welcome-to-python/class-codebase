4e766b4d4ba3b4e15ab911c002cc10df8f924547
Mdsahil01 <mohammedsahil3752@gmail.com>
2026-05-22 15:35:42 +0530

    Added q21 to q35 data structures programs

---
+++ students/sahil/Assignment_02/data_structures/q34_file_extension.py
@@ -0,0 +1,5 @@
+path = "D:/Workspace/Python/class-codebase/students/sahil/Assignment_02/data_structures/q34_file_extension.py"
+
+extension = path.split('.')[-1]
+
+print("File Extension:", extension)
